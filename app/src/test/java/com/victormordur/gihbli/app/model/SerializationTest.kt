2c8e1df0305dd5754953bc6b72a5ce36d5c120f9
Victor Moreno <victor.mordur@gmail.com>
2021-10-21 17:28:09 +0200

    Add watched boolean column to Film table

---
+++ app/src/test/java/com/victormordur/gihbli/app/model/SerializationTest.kt
@@ -0,0 +1,25 @@
+package com.victormordur.gihbli.app.model
+
+import com.victormordur.gihbli.app.data.model.Film
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.Assert
+import org.junit.Test
+
+class SerializationTest {
+
+    companion object {
+        val jsonParser = Json { encodeDefaults = true }
+    }
+
+    @Test
+    fun testWatchFieldNotSerialized() {
+        val watchedFilm =
+            Film("id", "title", "description", "releaseDate", "director", "imageURL", true)
+        val json = jsonParser.encodeToString(watchedFilm)
+        val deserializedFilm = jsonParser.decodeFromString(Film.serializer(), json)
+        Assert.assertTrue(json.contains("release_date"))
+        Assert.assertFalse(json.contains("watched"))
+        Assert.assertFalse(deserializedFilm.watched)
+    }
+}
