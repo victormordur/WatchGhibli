a4bfd9a98de0f170c35f8d5b979ef5b3a28f5057
Victor Moreno <victor.mordur@gmail.com>
2021-11-02 14:08:03 +0100

    Extract refreshable flow datastore functionality into abstract class

--- app/src/main/java/com/victormordur/gihbli/app/data/store/FilmRemoteDatastore.kt
+++ app/src/main/java/com/victormordur/gihbli/app/data/store/FilmRemoteDatastore.kt
@@ -3,32 +3,21 @@ package com.victormordur.gihbli.app.data.store
 import com.victormordur.gihbli.app.data.model.Film
 import com.victormordur.gihbli.app.data.service.remote.RemoteServiceContract
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.flow.onSubscription
-import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.withContext
 
 class FilmRemoteDatastore(
     private val service: RemoteServiceContract.FilmService,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) :
-    DatastoreContract.FilmRemote {
-    private val sharedFlow = MutableSharedFlow<List<Film>>()
-    private val upstreamFlow = MutableSharedFlow<List<Film>>().onSubscription {
-        emit(service.getAllFilms())
-    }
+    DatastoreContract.FilmRemote, RefreshableFlowDatastore<List<Film>>(dispatcher) {
 
-    override fun getAllFilmsFlow(): Flow<List<Film>> =
-        merge(sharedFlow, upstreamFlow).shareIn(CoroutineScope(dispatcher), SharingStarted.Eagerly)
+    override suspend fun onSubscription() = service.getAllFilms()
+    override suspend fun onRefresh() = service.getAllFilms()
+
+    override fun getAllFilmsFlow(): Flow<List<Film>> = getRefreshableFlow()
 
     override suspend fun refreshFilms() {
-        withContext(dispatcher) {
-            sharedFlow.emit(service.getAllFilms())
-        }
+        refresh()
     }
 }
