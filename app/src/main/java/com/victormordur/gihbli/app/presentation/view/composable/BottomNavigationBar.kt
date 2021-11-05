9d0da9452ed52686e556e2d850e16dfe4f924f54
Víctor Moreno <victor.mordur@gmail.com>
2021-11-05 14:36:02 +0100

    Update README

--- app/src/main/java/com/victormordur/gihbli/app/presentation/view/composable/BottomNavigationBar.kt
+++ app/src/main/java/com/victormordur/gihbli/app/presentation/view/composable/BottomNavigationBar.kt
@@ -1,13 +1,11 @@
 package com.victormordur.gihbli.app.presentation.view.composable
 
-import androidx.compose.foundation.background
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
